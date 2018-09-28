7e9502aeb47a1cd5367f9f68517023ef1bd21bca
Sangbn <sangbui2010@gmail.com>
2018-09-28 15:23:21 +0700

    home5

--- app/src/main/java/com/example/sangbn/homework_4/MainActivity.java
+++ app/src/main/java/com/example/sangbn/homework_4/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,11 +16,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v4.app.ActivityCompat;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -27,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "PermissionDemo";
     private static final int RECORD_REQUEST_CODE = 101;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private static String mCurrentPhotoPath;
+    public static int rotation = 0;
+
+    // your Final lat Long Values
+    Float Latitude = 0.0f;
+    Float Longitude = 0.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +49,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setupPermissions() {
-        int permission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Permission to record denied");
-            makeRequest();
-        }
-        else{
-            Toast.makeText(this, "asd", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    protected void makeRequest() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, RECORD_REQUEST_CODE);
+    public void onClick(View v) {
+        dispatchTakePictureIntent();
     }
 
     private boolean hasCamera() {
@@ -61,7 +58,20 @@ public class MainActivity extends AppCompatActivity {
                 PackageManager.FEATURE_CAMERA_ANY));
     }
 
-    static final int REQUEST_TAKE_PHOTO = 1;
+    private void setupPermissions() {
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied");
+            makeRequest();
+        } else {
+        }
+    }
+
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, RECORD_REQUEST_CODE);
+    }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -85,13 +95,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString());
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
-//            mImageView.setImageBitmap(imageBitmap);
+            getData(mCurrentPhotoPath);
+            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+            mImageView.setImageURI(Uri.parse(mCurrentPhotoPath));
         }
     }
 
@@ -110,26 +135,113 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onClick(View v){
-        dispatchTakePictureIntent();
+    private void getData(String uri) {
+        InputStream in = null;
+        try {
+            in = getContentResolver().openInputStream(Uri.fromFile(new File(uri)));
+            ExifInterface exifInterface = new ExifInterface(in);
+
+            int orientation = exifInterface.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotation = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotation = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotation = 270;
+                    break;
+            }
+            TextView textRotate = (TextView) findViewById(R.id.textRotate);
+            textRotate.setText("Rolation: " + rotation);
+
+            String LATITUDE = exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+            String LATITUDE_REF = exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
+            String LONGITUDE = exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+            String LONGITUDE_REF = exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
+
+            convert(LATITUDE, LONGITUDE, LATITUDE_REF, LONGITUDE_REF);
+
+            TextView textLatitude = (TextView) findViewById(R.id.textLatitude);
+            textLatitude.setText("Latitude: " + Latitude);
+            TextView textLongitude = (TextView) findViewById(R.id.textLongitude);
+            textLongitude.setText("Longitude: " + Longitude);
+
+            // Now you can extract any Exif tag you want
+            // Assuming the image is a JPEG or supported raw format
+        } catch (IOException e) {
+            // Handle any errors
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
     }
 
-    String mCurrentPhotoPath;
+    private void convert(String LATITUDE, String LONGITUDE, String LATITUDE_REF, String LONGITUDE_REF) {
+        if ((LATITUDE != null)
+                && (LATITUDE_REF != null)
+                && (LONGITUDE != null)
+                && (LONGITUDE_REF != null)) {
+            if (LATITUDE_REF.equals("N")) {
+                Latitude = convertToDegree(LATITUDE);
+            } else {
+                Latitude = 0 - convertToDegree(LATITUDE);
+            }
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+            if (LONGITUDE_REF.equals("E")) {
+                Longitude = convertToDegree(LONGITUDE);
+            } else {
+                Longitude = 0 - convertToDegree(LONGITUDE);
+            }
 
-        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()) ;
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
+        }
+    }
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
+    private Float convertToDegree(String stringDMS) {
+        Float result = null;
+        String[] DMS = stringDMS.split(",", 3);
+
+        String[] stringD = DMS[0].split("/", 2);
+        Float D0 = new Float(stringD[0]);
+        Float D1 = new Float(stringD[1]);
+        Float FloatD = D0 / D1;
+
+        String[] stringM = DMS[1].split("/", 2);
+        Float M0 = new Float(stringM[0]);
+        Float M1 = new Float(stringM[1]);
+        Float FloatM = M0 / M1;
+
+        String[] stringS = DMS[2].split("/", 2);
+        Float S0 = new Float(stringS[0]);
+        Float S1 = new Float(stringS[1]);
+        Float FloatS = S0 / S1;
+
+        result = new Float(FloatD + (FloatM / 60) + (FloatS / 3600));
+
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return (String.valueOf(Latitude)
+                + ", "
+                + String.valueOf(Longitude));
+    }
+
+    public int getLatitudeE6() {
+        return (int) (Latitude * 1000000);
+    }
+
+    public int getLongitudeE6() {
+        return (int) (Longitude * 1000000);
     }
 }
